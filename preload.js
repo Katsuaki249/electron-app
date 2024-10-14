0c97d1409ede6e70576c196d5bde1a0d63a747e0
Vinicius Shiroma <vinicius.katsuaki13@hotmail.com>
2024-10-14 16:23:30 -0300

    Adicionado inter-process comunication(IPC) no preload.js

--- preload.js
+++ preload.js
@@ -1,3 +1,12 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping'),
+});
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
